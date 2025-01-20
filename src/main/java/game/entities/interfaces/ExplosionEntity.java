334b68e977e829b35e6db008636fd05b572538a2
Nick Ziebert <zieben46@gmail.com>
2025-01-20 11:08:42 -0500

    remerging 2.0

--- src/main/java/game/entities/interfaces/ExplosionEntity.java
+++
@@ -1,9 +0,0 @@
-package game.entities.interfaces;
-
-public interface ExplosionEntity extends Entity {
-	
-	boolean isRendering();
-	
-	
-
-}
--- src/main/java/game/projectiles/projectilebehaviors/ProjectileBehavior.java
+++
@@ -1,9 +0,0 @@
-package game.projectiles.projectilebehaviors;
-import java.util.ArrayList;
-
-import game.entities.interfaces.ProjectileEntity;
-import game.entities.interfaces.ProjectileEntity.Team;
-
-public interface ProjectileBehavior {
-	public ArrayList<ProjectileEntity> shootBullet(int x, int y, int Vx, int Vy, Team Team);
-}
